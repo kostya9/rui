5435da073c675e100f3fb52a56b0865f801e8329
Kostiantyn Sharovarskyi <kostya9@gmail.com>
2022-06-19 22:38:35 +0300

    Replace version in package manifest

--- src/WinUi/find_version.ps1
+++
@@ -1,4 +0,0 @@
-$pattern = '[^\w]Version="((\d.?)+)"'
-$input_string = Get-Content Package.appxmanifest 
-$pattern_match = [regex]::Matches($input_string, $pattern)
-Write-Output $pattern_match.Groups[1].Value
